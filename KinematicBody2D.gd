b0ba66b91ec04dfbe18d640acd46379696df226f
BenTheHun1 <benhunsicker1@gmail.com>
2019-03-17 19:20:00 -0400

    New Sprites & animations

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 const SPEED = 70
 
 const movedir = Vector2(0,0)
-
+var lastmotion
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
@@ -20,4 +20,31 @@ func controls_loop():
 
 func movement_loop():
 	var motion = movedir.normalized() * SPEED
+	
 	move_and_slide(motion, Vector2(0,0))
+	if motion.x < 0:
+		$Sprite.play("walks")
+		$Sprite.flip_h = false
+		lastmotion = "l"
+	elif motion.x > 0:
+		$Sprite.play("walks")
+		$Sprite.flip_h = true
+		lastmotion = "r"
+	elif motion.y > 0:
+		$Sprite.play("walkd")
+		lastmotion = "d"
+	elif motion.y < 0:
+		$Sprite.play("walku")
+		lastmotion = "u"
+	else:
+		if lastmotion == "d":
+			$Sprite.play("idled")
+		elif lastmotion == "u":
+			$Sprite.play("idleu")
+		elif lastmotion == "l":
+			$Sprite.play("idles")
+			$Sprite.flip_h = false
+		elif lastmotion == "r":
+			$Sprite.play("idles")
+			$Sprite.flip_h = true
+		
